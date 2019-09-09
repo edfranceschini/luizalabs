45e629f08c307aa08cae11fb928e8e7fdea4a7a1
Edson Franceschini <fornecedor.vfranceschini@brasilprev.com.br>
2019-09-09 17:08:20 -0300

    Estrutura de models criada, e database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from datetime import datetime as dt
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
 api = Api(app)
 
 class ApiRoot(Resource):
@@ -17,6 +24,8 @@ class ApiRoot(Resource):
 def hello_world():
     return 'Hello World!'
 
+api.add_resource(ApiRoot, '/api/v1/')
+# api.add_resource()
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,41 @@
+from app import db
+from datetime import datetime as dt
+
+#  tabela auxiliar para relacionamentos Many-to-many
+favoritos = db.Table('favoritos',
+                     db.Column('cliente_id',
+                               db.Integer,
+                               db.ForeignKey('cliente.id'),
+                               primary_key=True),
+                     db.Column('produto_id',
+                               db.Integer,
+                               db.ForeignKey('produto.id'),
+                               primary_key=True)
+                     )
+
+#  tabela de clientes
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=False, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=dt.now)
+
+    def __repr__(self):
+        return '<Cliente %r>' % self.name
+
+    def get(self, id):
+        return Cliente.query.filter_by(id=id).first()
+
+
+    def favoritos(self):
+        pass
+
+#  tabela de produtos
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Produto %r>' % self.name
+
